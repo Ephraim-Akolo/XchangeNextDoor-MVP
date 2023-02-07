039324f9df862bf2545e19818394809c99f968fe
jake-ephraim <ephraimakolo2017@gmail.com>
2023-02-07 19:44:20 +0100

    created sql driver

---
+++ app/dbconnect.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from .settings import settings
+
+engine = create_async_engine(f"mysql+pymysql://{settings.xchangenextdoor_db_user}:{settings.xchangenextdoor_db_password}@{settings.xchangenextdoor_db_server}/{settings.xchangenextdoor_db_name}")
+
+Session = sessionmaker(bind=engine, autoflush=False)
+
+Base = declarative_base()
+
+
+def get_session():
+    local_session = Session()
+    try:
+        yield local_session
+    finally:
+        local_session.close()
