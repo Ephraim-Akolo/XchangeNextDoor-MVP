7d940c72268f02452e63937ed8c9cbc8c9ea3228
jake-ephraim <ephraimakolo2017@gmail.com>
2023-02-07 19:46:08 +0100

    updated create account

--- app/networks/etherium/ether.py
+++ app/networks/etherium/ether.py
@@ -1,5 +1,4 @@
-from .provider import web3
-
+from .provider import web3, create_account
 
 def get_latest_block():
     return web3.eth.block_number
@@ -10,11 +9,8 @@ def get_acct_balance(public_key:str, as_eth=False):
     else:
         return web3.eth.get_balance(public_key)
 
-def create_account(random_string:str="wHATS UP"):
-    r = web3.eth.account.create(random_string)
-    return r.address, r.key.hex()
-
 def send_ether(from_address:str, to_address:str, private_key:str, amount:float, max_gas_gwei:float=250, gas_fee_gwei:float=3.0, chainid=5):
+    to_address = web3.toChecksumAddress(to_address)
     balance = get_acct_balance(from_address, False)
     if balance < (web3.toWei(amount, 'ether') + web3.toWei(max_gas_gwei, "gwei")):
         return "insufficient ether!"
--- app/networks/etherium/provider.py
+++ app/networks/etherium/provider.py
@@ -1,11 +1,19 @@
 from web3 import Web3
 from ...settings import settings
 
-provider = Web3.HTTPProvider(settings.eth_web3_provider_uri)
+c = settings.eth_web3_provider_uri[0]
+if c.lower() == 'w':
+    _provider = Web3.WebsocketProvider(f"{settings.eth_web3_provider_uri[:6]}:{settings.eth_provider_api_key}@{settings.eth_web3_provider_uri[6:]}")
+else:
+    _provider = Web3.HTTPProvider(f"{settings.eth_web3_provider_uri[:8]}:{settings.eth_provider_api_key}@{settings.eth_web3_provider_uri[8:]}")
 
-web3 = Web3(provider)
+web3 = Web3(_provider)
 
 if web3.isConnected():
     print("connected to the etherium network successfully!")
 else:
-    raise Exception("Cannot connect to node provider!")
+    raise Exception("Cannot connect to etherium node provider!")
+
+def create_account(random_string:str="wHATS UP"):
+    r = web3.eth.account.create(random_string)
+    return r.address, r.key.hex()
